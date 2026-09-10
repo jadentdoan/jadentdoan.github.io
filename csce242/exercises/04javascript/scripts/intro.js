e5a532d54cc2a65069ec88d87db743b5cd8cab66
jadoan@email.sc.edu <jadoan@email.sc.edu>
2026-09-10 12:32:43 -0400

    Explored and exercised new JavaScript topics

--- csce242/exercises/04javascript/scripts/intro.js
+++ csce242/exercises/04javascript/scripts/intro.js
@@ -1,5 +1,17 @@
+
+//Shows a message when the button is clicked 
 document.getElementById("btn-show-message").onclick = (e) => {
     document.getElementById("p-message").innerHTML = "Hello, World!";
     e.target.innerHTML = "Message Shown";
 }
 
+//Styles the link when it is clicked
+document.getElementById("link").onclick = (e) => {
+    e.preventDefault(); //don't go to links destination
+    e.target.classlist.add("cool-link");
+}
+
+//When button is clicked, make the ball bounce
+document.getElementById("btn-bounce").onclick = (e) => {
+    document.getElementById("ball").classList.toggle("bouncing-ball");
+}
