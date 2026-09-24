df87de3941983a17657c1ac93c9df961a9a00566
jadentdoan <jadoan@email.sc.edu>
2026-09-24 01:59:20 -0400

    Recreated the whole wireframe homepage into an actual HTML and CSS file.. ~90% of the wireframe as a reference guide.

--- csce242/exercises/04javascript/scripts/intro.js
+++ csce242/exercises/04javascript/scripts/intro.js
@@ -1,46 +1,42 @@
-
-//Shows a message when the button is clicked 
+//Shows a message when the button is clicked
 document.getElementById("btn-show-message").onclick = (e) => {
-    document.getElementById("p-message").innerHTML = "Hello, World!";
-    e.target.innerHTML = "Message Shown";
-}
+    document.getElementById("p-message").innerHTML = "Hello World";
+    e.target.innerHTML = "done!";
+};
 
-//Styles the link when it is clicked
+//styles the link when it's clicked
 document.getElementById("link").onclick = (e) => {
     e.preventDefault(); //don't go to links destination
-    e.target.classlist.add("cool-link");
-}
+    e.target.classList.add("cool-link");
+};
 
-//When button is clicked, make the ball bounce
+//when button clicked make ball bounce
 document.getElementById("btn-bounce").onclick = (e) => {
     document.getElementById("ball").classList.toggle("bouncing-ball");
 }
 
-//When the user enters a number of days, show a message and change the image
-//Can do onchange or onkeyup. Onchange only fires when the user leaves the input box, onkeyup fires every time a key is pressed
+//when you change the number of days since you watered your plant
+//show a message and change the image
 document.getElementById("txt-num-days").onkeyup = (e) => {
     const numDays = parseInt(e.target.value);
     const pMessage = document.getElementById("p-plant-msg");
     const plantImage = document.getElementById("img-plant");
     plantImage.classList.remove("hidden");
     
-    if(numDays <= 2) {
-        pMessage.innerHTML = `Let your plant rest, it's only been ${numDays} days.`;
-        plantImage.src = "https://dummyimage.com/150x150/e800e8/00ff00&text=happy+plant";
-    }
-    else if(numDays <= 5) {
-        pMessage.innerHTML = `Time to water, it's been ${numDays} days.`;
-        plantImage.src = "https://dummyimage.com/150x150/e30000/00ff00&text=THIRSTY+plant";
-    }
-    else if(numDays <= 7){
-        pMessage.innerHTML = `Your plant is wilting, it's been ${numDays} days.`;
-    }
-    else {
-        pMessage.innerHTML = `Your plant is a goner`;
+    if(numDays <= 2){
+        pMessage.innerHTML = `Let your plant rest it's only been ${numDays} day(s).`;
+        plantImage.src="https://dummyimage.com/150x150/556B2F/fff&text=Happy+Plant";
+    } else if(numDays <= 5){
+        pMessage.innerHTML = `Time to water it's been ${numDays} days.`;
+        plantImage.src="https://dummyimage.com/150x150/BDB76B/fff&text=Thirsty";
+    } else if(numDays <= 7) {
+        pMessage.innerHTML = `Oh no your plant is wilting it's been ${numDays} days.`;
+    } else {
+        pMessage.innerHTML = "Your plant is a gooner";
     }
 }
 
-//Counting
+//counting
 let countInterval;
 let count = 0;
 const pCount = document.getElementById("p-count");
@@ -88,23 +84,24 @@ setInterval(()=>{
     const seconds = today.getSeconds();
     const minutes = today.getMinutes();
     const hours = today.getHours();
-    pDisplay.innerHTML = `${minutes}:${seconds} ${month}/${day}/${year}`;
+    pDisplay.innerHTML = `${hours}:${minutes}:${seconds} ${month}/${day}/${year}`;
 }, 1000);
 
 //toggle the navigation
-document.getElementById("toggle-nav").onclick = () => {
-    document.querySelector("main-nav ul").classList.toggle("hide-small")
+document.querySelector("#toggle-nav").onclick = () => {
+    document.querySelector("#main-nav ul").classList.toggle("hide-small");
 }
 
-//record the donation and fill up thermometer
+//record the users donation and fill up the thermometer appropriately
 const GOAL = 10000;
-document.getElementById("goal").innerHTML = GOAL
+document.getElementById("goal").innerHTML = GOAL;
 
 document.getElementById("btn-donation").onclick = () => {
     const userDonation = parseInt(document.getElementById("txt-donation").value);
     const donationP = document.getElementById("donation-message");
     percent = userDonation / GOAL * 100;
+    
+    donationP.innerHTML = `You are ${percent.toFixed(1)}% to your goal`;
+    document.querySelector(":root").style.setProperty("--donation", percent + "%");
 
-    donationP.innerHTML = `You are $(percent.toFixed(1))% to your goal`;
-    document.querySelector(":root").computedStyleMap.setProperty("--donation", percent + "%");
 }
--- csce242/exercises/04javascript/scripts/loops.js
+++ csce242/exercises/04javascript/scripts/loops.js
@@ -1,4 +1,4 @@
-//first loop ex
+//first loop example
 document.getElementById("btn-loop").onclick = (e) => {
     const loopResult = document.getElementById("loop-result");
 
@@ -9,44 +9,41 @@ document.getElementById("btn-loop").onclick = (e) => {
         p.classList.add("special");
         p.onclick = () => {
             console.log(`You clicked the ${i}'th element`);
-        }
-
-        /*You can put replace those inside lines with this
-        loopResult.innerHTML += `<p>$(i+1)</p>
-        */
+        };
     }
+/*
+     for(let i = 0; i < 10; i++){
+        loopResult.innerHTML += `<p>${i+1}</p>`;
+    }*/
 };
 
 //looping through a range
-document.getElementById("btn-range").onclick = () => {
-    const startText = document.getElementById("txt-start").value;
-    const endText = document.getElementById("txt-end").value;
+document.getElementById("btn-loop-range").onclick = () => {
+    const startNum = parseInt(document.getElementById("txt-start").value);
+    const endNum = parseInt(document.getElementById("txt-end").value);
     const errorStart = document.getElementById("error-start");
     errorStart.classList.add("hidden");
     const errorEnd = document.getElementById("error-end");
     errorEnd.classList.add("hidden");
     const ul = document.getElementById("range-list");
 
-    if(isNaN(startText) || startText < 0 || startText > 5){
+    if(startNum < 0 || startNum > 5){
         errorStart.innerHTML = "* Invalid";
         errorStart.classList.remove("hidden");
         return;
     }
 
-    if(isNaN(endText) || endText < 0 || endText > 5 || endText < startText){
+    if(endNum < 10 || endNum > 20 || endNum < startNum){
         errorEnd.innerHTML = "* Invalid";
         errorEnd.classList.remove("hidden");
         return;
     }
 
-    for(let i = parseInt(startText); i < parseInt(endText); i++){
+    ul.innerHTML = "";
+
+    for(let i = startNum; i < endNum; i++){
         const li = document.createElement("li");
         li.innerHTML = i;
         ul.appendChild(li);
     }
 };
-
-//first array
-document.getElementById("btn-show-toys").onclick = () => {
-    
-}
